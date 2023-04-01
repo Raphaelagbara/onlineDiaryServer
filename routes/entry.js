f2c8ab8485f988f1321bc0c538b555ef16da30a7
Raphael Agbara <ralphagbara@email.com>
2023-04-01 11:14:44 +0100

    added middleware and some refactoring

--- routes/entry.js
+++ routes/entry.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const { getEntries, createEntries } = require("../controllers/entries");
+const {
+  getEntries,
+  createEntries,
+  updateEntry,
+  deleteEntry,
+  likePost,
+} = require("../controllers/entries");
 const app = express();
 const router = express.Router();
 
 //defining the routes
 router.get("/", getEntries);
 router.post("/", createEntries);
+router.get("/:id", getEntries);
+router.patch("/:id", updateEntry);
+router.delete("/:id", deleteEntry);
+router.patch("/:id/likePost", likePost);
 
 module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+import { signin, signup } from "../controllers/user.js";
+
+router.post("/signin", signin);
+router.post("/signup", signup);
+
+export default router;
